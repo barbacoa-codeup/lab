a9140aace5ced5bbda0994506e39b88e6a6f27b8
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-17 16:53:07 -0600

    added second week exercises

---
+++ chaney-second-week.js
@@ -0,0 +1,8 @@
+//https://javarevisited.blogspot.com/2011/06/top-programming-interview-questions.html
+
+// 1) Write code to check a String is palindrome or not?
+
+//2) Write a method which will remove any given character from a String?
+// hint: you can remove a given character from String by converting it into a character array and then using substring() method for removing them from output string.
+
+// 3) Print all permutation of String both iterative and Recursive way? (solution)
