bb00c2bc668a068c1f6484e83071e4b148c35270
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-20 18:45:20 -0600

    added three problems

--- chaney-second-week.js
+++ chaney-second-week.js
@@ -2,7 +2,21 @@
 
 // 1) Write code to check a String is palindrome or not?
 
+function isPalindrome(str) {
+    if (typeof(str) === 'string')
+        return str === str.split('').reverse().join('');
+}
+
+// isPalindrome('racecar');
+// isPalindrome('element');
+// isPalindrome(4);
+// isPalindrome('eye');
+
 //2) Write a method which will remove any given character from a String?
 // hint: you can remove a given character from String by converting it into a character array and then using substring() method for removing them from output string.
+
+function removeChar(str) {
+    
+}
 
 // 3) Print all permutation of String both iterative and Recursive way? (solution)
