416fcff4410f30f259764591471ddb41f50204f7
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-22 14:01:55 -0600

    completed second problem

--- chaney-second-week.js
+++ chaney-second-week.js
@@ -3,7 +3,10 @@
 // 1) Write code to check a String is palindrome or not?
 
 function isPalindrome(str) {
-    if (typeof(str) === 'string')
+    // if the input is a string
+    if (typeof (str) === 'string')
+        // return true if the string is equal to:
+        // string -> array of letters -> reverse the order -> rejoin array as new string
         return str === str.split('').reverse().join('');
 }
 
@@ -15,15 +18,25 @@ function isPalindrome(str) {
 //2) Write a method which will remove any given character from a String?
 // hint: you can remove a given character from String by converting it into a character array and then using substring() method for removing them from output string.
 
-
-function removeChar(str, character) {
-    let letterArray= str.toLowerCase().split('');
-    console.log(letterArray);
-    let char = character.toLowerCase();
-    console.log(char);
-    return letterArray.replace(char, "")
+function removeChar(str, char) {
+    // puts string to lowercase, separates letters into array
+    let letterArray = str.toLowerCase().split('');
+    // puts designated character to lowercase
+    let character = char.toLowerCase();
+    // sets blank array to push letters that are not equal to character
+    let newArray = [];
+    for (let i = 0; i < letterArray.length; i++) {
+        //if the letter at index i does not equal the designated character
+        if (letterArray[i] !== character)
+            //push the corresponding letter into the blank array
+            newArray.push(letterArray[i]);
+    }
+    // join the new array without the designated character into a string
+    return newArray.join('')
 }
 
-removeChar('banana', 'a');
+// removeChar('banana', 'a');
+// removeChar('oh okay', 'o');
 
-// 3) Print all permutation of String both iterative and Recursive way? (solution)
+// 3) Print all permutation of String both iterative and Recursive way?
+
